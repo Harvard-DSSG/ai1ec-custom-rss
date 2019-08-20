ae2c534c30153396693dc998f7059f80cef58dbf
Cole Crawford <cole_crawford@harvard.edu>
2019-08-20 14:56:50 -0400

    First push

---
+++ ai1ec-custom-rss/ai1ec-custom-rss.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: All in One Timely Calendar Custom RSS
+ * Description: For DSSG RSS feed
+ * Version: 1.0
+ * Author: Cole Crawford
+ */
+
+ // add_action('init', 'customRSS');
+ // function customRSS(){
+ //         add_feed('dssg', 'customRSSFunc');
+ // }
+
+//  function customRSSFunc(){
+//         get_template_part('rss', 'feedname');
+// }
+
+// public function setup_rewrites(){
+//         $url_slug = 'dssg-rss';
+//         add_rewrite_rule( $url_slug . '/?$', 'index.php?custom_plugin=index', 'top' );
+//         add_rewrite_rule( $url_slug . '/page/([0-9]{1,})/?$', 'index.php?custom_plugin=items&custom_plugin_paged=$matches[1]', 'top' );
+//         add_rewrite_rule( $url_slug . '/([a-zA-Z0-9\-]{1,})/?$', 'index.php?custom_plugin=detail&custom_plugin_vehicle=$matches[1]', 'top' );
+// }
